f64925019e92d4c381a66f7558d85bb302cdcc6c
cpucorecore <cpucorecore@gmail.com>
2022-11-11 14:06:07 +0800

    update sh

--- scripts/test_plan_add.sh
+++ scripts/test_plan_add.sh
@@ -1,16 +1,23 @@
+to=1000
+tag=crdt
+bs=$((1024*1024))
+
 for thread in `cat threads`
 do
 echo "____thread${thread}_____"
-./ipfs_benchmark -g ${thread} --to 20000 -w 100 --tag crdt --ts=false cluster add --bs $((1024*1024))
+
+./ipfs_benchmark -g ${thread} --to ${to} -w 100 --tag ${tag} cluster add --bs ${bs}
+
 sleep 14400
 date
 
-./ipfs_benchmark -g 2 -w 500 --to 20000 --trf test_result/ClusterAdd_0-20000_g${thread}_bs1048576_r2-2_crdt.json --tag crdt --ts=false cluster pin rm
-sleep 300
+
+./ipfs_benchmark --tag crdt --to ${to} -w 100 -g 2 cluster pin --trf tests/reports/ClusterAdd_0-${to}_g${thread}_bs${bs}_r2-2_${tag}.json rm
+sleep 3000
 
 date
 ./ipfs_benchmark gc
 sleep 720
-echo "@@@@thread${i}@@@@"
+echo "@@@@thread${thread}@@@@"
 
 done
